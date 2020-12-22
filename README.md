6a96ee52b2b6a9b52da8ad7be051966f70b58aaa
g-gutierrez <grgutz@gmail.com>
2020-12-21 22:34:45 -0800

    Finishing assignment

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# ucsd-homework-09
+
+  # Project Title : test9
+  
+  ## Project Description:
+  test8
+  
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributors](#contributors)
+  * [Test](#test)
+  * [License](#license)
+  * [Author](#Author)
+  * [Email](#Email)
+  * [Badges](#badges)
+  
+  ## Installation
+  test7
+  
+  ## Usage
+  test6
+  
+  ## Contributing
+  test5
+  
+  ## Test
+  test4
+  
+  ## License
+  Apache
+  
+  ## Author 
+  g-gutierrez
+
+  ## Author Avatar
+  ![GitHub profile pic](https://avatars1.githubusercontent.com/u/16087425?v=4)
+  
+  ## Email
+  grgutz@gmail.com
+
+  ## Badges
+  
+  ![Size](https://img.shields.io/github/repo-size/g-gutierrez/ucsd-homework-09)
+  
+  
---
+++ index.js
@@ -0,0 +1,134 @@
+const inquirer = require('inquirer');
+const axios = require('axios');
+const fs = require('fs');
+
+const questions = [
+    {
+      type: 'input',
+      message: 'Enter your project title: ',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Enter your project description: ',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Provide installation instructions: ',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Provide usage information: ',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Enter contributors: ',
+      name: 'contributors',
+    },
+    {
+      type: 'input',
+      message: 'Enter test instructions: ',
+      name: 'tests',
+    },
+    {
+      type: 'checkbox',
+      message: 'Please select a license: ',
+      choices: [
+        "Apache",
+        "MIT"
+      ],
+      name: "license"
+    },
+    {
+      type: 'input',
+      message: 'What is your github username: ',
+      name: 'username'
+    },
+    {
+      type: 'input',
+      message: 'What is your email address: ',
+      name: 'email'
+    },
+    {
+      type: 'input',
+      message: 'What is your github repo: ',
+      name: 'repo'
+    }
+  ];
+
+  function init() {
+    inquirer.prompt(questions).then(answers => {
+      var filename = "README.md";
+      axios
+        .get("https://api.github.com/users/" + answers.username)
+        .then(response => {
+          var imageURL = response.data.avatar_url;
+          answers.image = imageURL;
+          writeToFile(filename, answers);
+        });
+    });
+  };
+
+
+  function writeToFile(filename, data) {
+    fs.writeFile(filename, genMD(data), function(err) {
+      if (err) {
+        throw err;
+      }
+    });
+  };
+
+
+  function genMD(data) {
+    return `
+  # Project Title : ${data.title}
+  
+  ## Project Description:
+  ${data.description}
+  
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributors](#contributors)
+  * [Test](#test)
+  * [License](#license)
+  * [Author](#Author)
+  * [Email](#Email)
+  * [Badges](#badges)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Contributing
+  ${data.contributors}
+  
+  ## Test
+  ${data.tests}
+  
+  ## License
+  ${data.license}
+  
+  ## Author 
+  ${data.username}
+
+  ## Author Avatar
+  ![GitHub profile pic](${data.image})
+  
+  ## Email
+  ${data.email}
+
+  ## Badges
+  
+  ![Size](https://img.shields.io/github/repo-size/${data.username}/${data.repo})
+  
+  `;
+  };
+
+  init();
+
